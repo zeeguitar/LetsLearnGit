de497120539ddcf5c76a7d1864faad40c8dfd329
Zee <mailing.zeeshaan@gmail.com>
2023-01-16 09:36:32 +0530

    Added Multi.java

---
+++ Multi.java
@@ -0,0 +1,11 @@
+public class Multi
+{
+    Run|Debug
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=6;
+
+        System.out.println("The Product after Multiplication is" + (a*b));
+    }
+}
