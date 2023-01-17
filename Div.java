e138e2a013bbdffe4caee3d75ba849c1011949c7
Zee <mailing.zeeshaan@gmail.com>
2023-01-17 08:52:43 +0530

    Adding Div.java

---
+++ Div.java
@@ -0,0 +1,11 @@
+public class Div
+{
+    Run|Debug
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+
+        System.out.println("The Quotient after Division is" + (a/b));
+    }
+}
